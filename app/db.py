e3a03c45cc9f3ea98d8164bc0ed49d73c7c8338a
miqbalgithub <86734925+miqbalgithub@users.noreply.github.com>
2023-01-04 15:58:26 +0700

    Update db.py

--- app/db.py
+++ app/db.py
@@ -8,7 +8,7 @@ load_dotenv()  # take environment variables from .env.
 # Mysql Settings
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER') or 'dbuser'
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD') or 'dbuser123'
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') or '172.30.102.176' # localhost
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') or '172.30.157.26' # localhost
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB') or 'sampledb'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
